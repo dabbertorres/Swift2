2f8163d869d6868412e3054cb91c3a8aaac04054
Alec Iverson <adiverso93@gmail.com>
2014-04-06 12:36:23 -0700

    Integrated Script into AssetManager
    Modified Script to be more friendly with the AssetManager, by adding a loadFromFile function. This will also make adding loadFromMemory and such probable as well.

---
+++ src/Scripting/Script.cpp
@@ -0,0 +1,43 @@
+#include "Script.hpp"
+
+namespace swift
+{
+	Script::Script()
+	{
+		deleteMe = false;
+		
+		// We don't want to give the scripts access to os commands or file writing
+		luaState.OpenLib("base", luaopen_base);
+		luaState.OpenLib("math", luaopen_math);
+		luaState.OpenLib("string", luaopen_string);
+		luaState.OpenLib("table", luaopen_table);
+	}
+
+	Script::~Script()
+	{
+	}
+
+	bool Script::loadFromFile(const std::string& file)
+	{
+		bool r = luaState.Load(file);	// r will be false if errors, true otherwise
+
+		luaState["Start"]();
+		return r;
+	}
+
+	void Script::run()
+	{
+		luaState["Update"]();
+
+		if(bool(luaState["Done"]) == true)
+		{
+			luaState["Finish"]();
+			deleteMe = true;
+		}
+	}
+
+	bool Script::toDelete()
+	{
+		return deleteMe;
+	}
+}
