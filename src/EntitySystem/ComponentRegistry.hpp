6b310311b5176227c39e44c63af8689d2b1fbaf9
Alec Iverson <adiverso93@gmail.com>
2014-09-15 20:27:52 -0700

    Added a file to translate strings to Components

---
+++ src/EntitySystem/ComponentRegistry.hpp
@@ -0,0 +1,30 @@
+#ifndef COMPONENTREGISTRY_HPP
+#define COMPONENTREGISTRY_HPP
+
+#include <unordered_map>
+
+#include "Component.hpp"
+#include "Components/Drawable.hpp"
+#include "Components/Movable.hpp"
+#include "Components/Physical.hpp"
+
+namespace swift
+{
+	class ComponentRegistry
+	{
+		public:
+			static Component* get(const std::string& c)
+			{
+				if(c == "Drawable")
+					return new Drawable;
+				else if(c == "Movable")
+					return new Movable;
+				else if(c == "Physical")
+					return new Physical;
+				else
+					return nullptr;
+			}
+	};
+}
+
+#endif // COMPONENTREGISTRY_HPP
