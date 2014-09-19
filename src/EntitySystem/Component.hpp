dbffd4247734f6575b065875c445b8f1e7447c11
Alec Iverson <adiverso93@gmail.com>
2014-09-18 22:14:57 -0700

    Made componenets Observable

--- src/EntitySystem/Component.hpp
+++ src/EntitySystem/Component.hpp
@@ -1,11 +1,13 @@
 #ifndef COMPONENT_HPP
 #define COMPONENT_HPP
 
+#include "../MessageSystem/Observable.hpp"
+
 #include <string>
 
 namespace swift
 {
-	class Component
+	class Component : public Observable
 	{
 		public:
 			static std::string getType();
