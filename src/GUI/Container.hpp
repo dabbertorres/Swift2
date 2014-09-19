226bf684bffa77dfaff2368d3ad08fe883bc27e5
Alec Iverson <adiverso93@gmail.com>
2014-09-19 10:28:47 -0700

    Removed hopefully no longer needed debug drawing code.

--- src/GUI/Container.hpp
+++ src/GUI/Container.hpp
@@ -5,7 +5,6 @@
 
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/Window/Event.hpp>
-//#include <SFML/Graphics/RectangleShape.hpp>
 
 #include "Widget.hpp"
 
@@ -100,15 +99,6 @@ namespace cstr
 		private:
 			void draw(sf::RenderTarget& target, sf::RenderStates states) const
 			{
-				// debug drawing
-				/*sf::RectangleShape border({static_cast<float>(rect.width), static_cast<float>(rect.height)});
-				border.setFillColor(sf::Color::Transparent);
-				border.setOutlineColor(sf::Color::Red);
-				border.setOutlineThickness(1);
-				border.setPosition(rect.left, rect.top);
-				
-				target.draw(border);*/
-				
 				if(isVisible)
 				{
 					for(auto& w : widgets)
