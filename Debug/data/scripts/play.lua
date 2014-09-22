0163de32ae0b1bd11e6f21fa36ce69f6e5ad574f
Alec Iverson <adiverso93@gmail.com>
2014-09-21 20:35:40 -0700

    Testing bindings to EntitySystem

--- Debug/data/scripts/play.lua
+++ Debug/data/scripts/play.lua
@@ -1,5 +1,23 @@
 --Play setup
 function Start()
+	e = newEntity()
+	
+	e:add("Drawable")
+	e:add("Movable")
+	e:add("Physical")
+	
+	if e:has("Drawable") then
+		print("e has Drawable")
+	end
+	
+	if e:has("Movable") then
+		print("e has Movable")
+	end
+	
+	if e:has("Physical") then
+		print("e has Physical")
+	end
+	
 	Done = false
 end
 
