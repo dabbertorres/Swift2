c7e29da08c6b77e276c960fe05643300c1677c56
Alec Iverson <adiverso93@gmail.com>
2014-09-27 20:24:36 -0700

    ECS -> Lua works! Entities are now created and managed via Lua. Moved map bounding of entities to Update. Created an Entity in Lua.

--- Debug/data/scripts/play.lua
+++ Debug/data/scripts/play.lua
@@ -1,26 +1,40 @@
 --Play setup
 function Start()
-	e = newEntity()
+	winSizeX, winSizeY = getWindowSize()
+	
+	local e = newEntity()
 	
 	e:add("Drawable")
 	e:add("Movable")
 	e:add("Physical")
+	e:add("Name")
 	
-	if e:has("Drawable") then
-		print("e has Drawable")
-	end
-	
-	if e:has("Movable") then
-		print("e has Movable")
-	end
-	
-	if e:has("Physical") then
-		print("e has Physical")
-	end
+	setTexture(e:getDrawable(), "./data/textures/ship.png")
+	setMoveVelocity(e:getMovable(), 100)
+	setPosition(e:getPhysical(), 400, 20)
+	setSize(e:getPhysical(), getSpriteSize(e:getDrawable()))
+	setName(e:getName(), "Lua Entity")
 	
 	Done = false
 end
 
 function Update()
-
+	for i = 0, getTotalEntities() - 1 do
+		local e = getEntity(i)
+		local p = e:getPhysical()
+		local x, y = getPosition(p)
+		local w, h = getSize(p)
+		
+		if x < 0 then
+			setPosition(p, 0, y)
+		elseif x + w > winSizeX then
+			setPosition(p, winSizeX - w, y)
+		end
+		
+		if y < 0 then
+			setPosition(p, x, 0)
+		elseif y + h > winSizeY then
+			setPosition(p, x, winSizeY - h)
+		end
+	end
 end
