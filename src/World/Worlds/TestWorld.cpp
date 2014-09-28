a4f408dfd14fdf2084a00c3b32873f5294126eea
Alec Iverson <adiverso93@gmail.com>
2014-09-27 20:16:28 -0700

    Removed world bounding code.

--- src/World/Worlds/TestWorld.cpp
+++ src/World/Worlds/TestWorld.cpp
@@ -19,18 +19,6 @@ namespace swift
 			physicalSystem.update(*e, dt);
 			if(e->has<Physical>())
 			{
-				/*Physical* phys = e->get<Physical>();
-				// bound x position to width
-				if(phys->position.x < 0)
-					phys->position.x = 0;
-				else if(phys->position.x + phys->size.x > size.x)
-					phys->position.x = size.x - phys->size.x;
-				// check y position to height
-				if(phys->position.y < 0)
-					phys->position.y = 0;
-				else if(phys->position.y + phys->size.y > size.y)
-					phys->position.y = size.y - phys->size.y;
-				*/
 				// simple crappy world collision
 				/*if(tilemap.getTileNum(static_cast<int>(phys->position.x / tilemap.getTileSize().x) + static_cast<int>(phys->position.y / tilemap.getTileSize().y) * tilemap.getSize().x) == 4 ||
 					tilemap.getTileNum(static_cast<int>((phys->position.x + phys->size.x) / tilemap.getTileSize().x) + static_cast<int>((phys->position.y + phys->size.y) / tilemap.getTileSize().y) * tilemap.getSize().x) == 4 ||
