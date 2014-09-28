aceca4a9dfd7c0e0fd7ab9831bb7d55620d7c833
Alec Iverson <adiverso93@gmail.com>
2014-09-27 20:23:53 -0700

    Added new Name component to Registry

--- src/EntitySystem/ComponentRegistry.hpp
+++ src/EntitySystem/ComponentRegistry.hpp
@@ -5,6 +5,7 @@
 #include "Components/Drawable.hpp"
 #include "Components/Movable.hpp"
 #include "Components/Physical.hpp"
+#include "Components/Name.hpp"
 
 namespace swift
 {
@@ -19,6 +20,8 @@ namespace swift
 					return new Movable;
 				else if(c == "Physical")
 					return new Physical;
+				else if(c == "Name")
+					return new Name;
 				else
 					return nullptr;
 			}
