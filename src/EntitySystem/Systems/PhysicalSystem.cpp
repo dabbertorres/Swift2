075e2962dc422fe9c4b79e37ad0da39907072a4e
Alec Iverson <adiverso93@gmail.com>
2014-09-30 11:39:17 -0700

    Removed the distance function and moved it to World

--- src/EntitySystem/Systems/PhysicalSystem.cpp
+++ src/EntitySystem/Systems/PhysicalSystem.cpp
@@ -2,8 +2,6 @@
 
 #include "../Components/Physical.hpp"
 
-#include <cmath>
-
 namespace swift
 {
 	void PhysicalSystem::update(Entity& entity, float /*dt*/)
@@ -12,10 +10,5 @@ namespace swift
 		{
 			// collision handling code
 		}
-	}
-	
-	float PhysicalSystem::distance(sf::Vector2f one, sf::Vector2f two) const
-	{
-		return std::sqrt(std::pow(two.x - one.x, 2) + std::pow(two.y - one.y, 2));
 	}
 }
