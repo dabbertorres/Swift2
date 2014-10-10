c815f56d34a62d74c6e57fbfe7f9675c1fc39cb7
Alec Iverson <adiverso93@gmail.com>
2014-10-10 14:02:52 -0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,12 +6,22 @@ $(document).ready(function()
 		{
 			height: "+=50"
 		}, 500);
+		
+		$(this "a").animate(
+		{
+			top: "+=50"
+		}, 500);
 	});
 	
 	$("#forkme_banner").mouseleave(function()
 	{
 		$(this).animate({
 			height: "-=50"
+		}, 500);
+		
+		$(this "a").animate(
+		{
+			top: "+=50"
 		}, 500);
 	});
 });
