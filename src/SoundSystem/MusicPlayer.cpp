13e4cc68836f7712bf1ef9ef2968c68d225ca2c0
Alec Iverson <adiverso93@gmail.com>
2014-11-05 14:07:31 -0700

    Fixed an out of bounds error

--- src/SoundSystem/MusicPlayer.cpp
+++ src/SoundSystem/MusicPlayer.cpp
@@ -4,20 +4,23 @@ namespace swift
 {
 	MusicPlayer::MusicPlayer()
 		:	activeTrack(0),
-			volume(100)
+		    volume(100)
 	{
 	}
 
 	void MusicPlayer::update()
 	{
-		if(musics[activeTrack]->getStatus() == sf::Music::Status::Stopped && musics.size() != 0)
+		if(musics.size() > 0)
 		{
-			activeTrack++;
-			
-			if(activeTrack >= musics.size())
-				activeTrack = 0;
-			
-			musics[activeTrack]->play();
+			if(musics[activeTrack]->getStatus() == sf::Music::Status::Stopped && musics.size() != 0)
+			{
+				activeTrack++;
+
+				if(activeTrack >= musics.size())
+					activeTrack = 0;
+
+				musics[activeTrack]->play();
+			}
 		}
 	}
 
@@ -26,33 +29,33 @@ namespace swift
 		if(musics[activeTrack]->getStatus() != sf::Music::Status::Playing && musics.size() != 0)
 			musics[activeTrack]->play();
 	}
-	
+
 	void MusicPlayer::pause()
 	{
 		if(musics.size() != 0)
 			musics[activeTrack]->pause();
 	}
-	
+
 	void MusicPlayer::stop()
 	{
 		if(musics.size() != 0)
 		{
 			musics[activeTrack]->stop();
 			activeTrack = 0;
-			
+
 			SoundsLimit::total -= musics.size();
-			
+
 			musics.clear();
 		}
 	}
-	
+
 	void MusicPlayer::setVolume(float v)
 	{
 		for(auto& m : musics)
 		{
 			m->setVolume(v);
 		}
-		
+
 		volume = v;
 	}
 
@@ -60,14 +63,14 @@ namespace swift
 	{
 		if(SoundsLimit::total + 1 > SoundsLimit::limit)
 			return false;
-		
+
 		musics.emplace_back(&m);
 		musics.back()->setLoop(loop);
 		musics.back()->setVolume(volume);
 		musics.back()->setRelativeToListener(true);
-		
+
 		SoundsLimit::total++;
-		
+
 		return true;
 	}
 }
